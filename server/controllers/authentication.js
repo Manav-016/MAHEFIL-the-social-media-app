db4c58f0f52cbdc478e514da45215c7c18898732
Manav-016 <mmanav169@gmail.com>
2023-02-08 10:24:48 +0530

    authentication done

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,49 +1,68 @@
-import bcrypt from 'bcrypt';
-import jsw from 'jsonwebtoken';
-import User from '../models/User.js'
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 export const register = async (req, res) => {
-    console.log("------17------");
-    console.log(req.body);
-    try {
-        const {
-            firstName,
-            lastName,
-            email,
-            password,
-            picturePath,
-            friends,
-            location,
-            occupation,
-        } = req.body;
-        console.log("------17------");
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt);
+  console.log("....");
 
-        console.log("------20------");
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
 
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 1000),
+      impressions: Math.floor(Math.random() * 1000),
+    });
 
+    const savedUser = await newUser.save();
 
-        const newUser = new User({
-            firstName,
-            lastName,
-            email,
-            password: passwordHash,
-            picturePath,
-            friends,
-            location,
-            occupation,
-            viewedProfile: Math.floor(Math.random() * 1000),
-            impressions: Math.floor(Math.random() * 1000),
-        })
+    res.status(200).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-        const savedUser = await newUser.save();
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-        res.status(200).json(savedUser);
+    const user = await User.findOne({ email: email });
 
+    if (!user) {
+      return res.status(400).json({ error: "User Does Not Exists" });
     }
-    catch (error) {
-        res.status(500).json({ error: error.message });
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid Credentials" });
     }
-}
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+
+    delete user.password;
+
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-import bodyParser from 'body-parser';
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import helmet, { crossOriginResourcePolicy } from 'helmet';
-import morgan from 'morgan';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import multer from 'multer';
-import { register } from './controllers/authentication.js';
+import bodyParser from "body-parser";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet, { crossOriginResourcePolicy } from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import multer from "multer";
+import { register } from "./controllers/authentication.js";
+import authUser from "./routes/authentication.js";
 
 /*CONF*/
 
@@ -20,48 +21,52 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({
-    limit: "10MB", extended: true
-}));
-app.use(bodyParser.urlencoded({
-    limit: "10MB", extended: true
-}));
+app.use(
+  bodyParser.json({
+    limit: "10MB",
+    extended: true,
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    limit: "10MB",
+    extended: true,
+  })
+);
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /*file*/
 
-const storage = multer.diskStorage(
-    {
-        destination: function (req, file, callback) {
-            callback(null, 'public/assets');
-        },
-        filename: function (req, file, callback) {
-            callback(null, file.originalname);
-        }
-
-    }
-);
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "public/assets");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
 const upload = multer({ storage });
 
-
-/*routs*/
+/*upload routs*/
 
 app.post("/auth/register", upload.single("picture"), register);
 
+/*routs*/
+
+app.use("/auth", authUser);
 
 /*mongo*/
 
 const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`${PORT} is being listened`);
-    })
-}).catch((error) => console.log(`${error}`));
-
-
-
-
+      console.log(`${PORT} is being listened`);
+    });
+  })
+  .catch((error) => console.log(`${error}`));
